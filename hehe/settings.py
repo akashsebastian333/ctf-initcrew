36ad789314629cc00aa9f33ba7275eb8182068a4
AkashSebastian <31592003+akashsebastian333@users.noreply.github.com>
2023-05-12 19:27:04 +0530

    Update settings.py

--- hehe/settings.py
+++ hehe/settings.py
@@ -15,7 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
+SESSION_SERIALIZER = 'django.contrib.sessions.serializers.PickleSerializer'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
