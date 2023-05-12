2099df50bbb9efc79776b98d908f2f457330a0de
AkashSebastian <31592003+akashsebastian333@users.noreply.github.com>
2023-05-12 21:05:18 +0530

    Update settings.py

--- hehe/settings.py
+++ hehe/settings.py
@@ -82,14 +82,10 @@ WSGI_APPLICATION = 'hehe.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'TEST': {
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
