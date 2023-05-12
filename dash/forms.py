75b52773fc9d020a52ca6a15236395244f811e86
AkashSebastian <31592003+akashsebastian333@users.noreply.github.com>
2023-05-12 19:32:40 +0530

    Update forms.py

--- dash/forms.py
+++ dash/forms.py
@@ -6,7 +6,7 @@ from .models import Message
 from django.contrib.auth.models import User
 from django import forms
 from django.contrib.auth.models import User
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from .models import Profile
 
