d5b93559774a2f13ab7178cf0e2bd89b26a052e3
AkashSebastian <31592003+akashsebastian333@users.noreply.github.com>
2023-05-12 21:19:12 +0530

    Update settings.py

--- hehe/settings.py
+++ hehe/settings.py
@@ -86,6 +86,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'TEST': {
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
 }
 
